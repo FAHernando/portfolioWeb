58373ba9105da8aa716b017ad7c70d75cfc3272d
FAHernando <alexishernando91@gmail.com>
2022-04-28 18:01:12 -0300

    Proyecto subido

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { PieComponent } from './componentes/pie/pie.component';
+import {HttpClientModule} from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    EstudiosComponent,
+    AcercaDeComponent,
+    FormularioComponent,
+    PieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
